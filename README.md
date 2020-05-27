812a0e9a75ab9be7b41adf67e22335dca9695ec8
azware <anh.blackoni@gmail.com>
2020-05-27 11:33:10 +0700

    commit on master

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/59108d73-7d53-4a27-a23b-02d086617463/deploy-status)](https://app.netlify.com/sites/azware-gats/deploys)
 
+## MASIH DALAM TAHAP PENGEMBANGAN
 
 Project ini saya bangun menggunakan Gatsby Starter Default untuk saya gunakan sebagai bahan latihan. Kedepannya saya akan membuat berbagai project yang sudah pernah saya bangun menggunakan PHP dan akan saya ubah ke mode Javascript murni menggunakan berbagai macam framework javascript. Untuk saat ini, belajar dulu pakai Gatsby.
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,58 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Button } from 'react-bootstrap'
 import { FaCoffee } from 'react-icons/fa'
 //import { _ } from "underscore" // belum tau cara makenya. bgg jg. harus blajr underscore 
 
 import Layout from "../components/layout"
 import Himage from "../components/images/image" // masih bgg klo importnya pake _ error
-import Logo from "../components/images/image"
+import Logo from "../components/images/logo"
 import SEO from "../components/seo"
+
+// For React-Bootstrap 
+import { Button } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+import { Alert } from 'react-bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{maxWidth: '200px', paddingbottom: '2rem'}}>
-      <Logo />
-    </div>
-    <h1>Hai.... <FaCoffee /></h1>
-    <Button variant="primary" size="sm">Check this!</Button> <br/>
-    <p>Selamat datang di web portfolio azware menggunakan gatsby</p>
-    <p>Berikut tampilkan beberapa contoh sistem yang telah kami kerjakan</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Himage />
-    </div>
-    <Link to="/project/" style={{ marginRight: '20px' }} className="Button" variant="primary">Project</Link>
+    
+    <Container>
+      <Row>
+        <Col sm={2}>
+          <div style={{maxWidth: '100px'}}>
+            <Logo />
+          </div>
+        </Col>
+        <Col sm={10}>
+          <h1>Hai.... <FaCoffee /></h1>
+          <Button variant="primary" size="sm">Check Btn!</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12}>        
+          <Alert variant="info">
+            <Alert.Heading>Hey, nice to see you</Alert.Heading>
+            <p>
+              Selamat datang di web portfolio azware menggunakan gatsby              
+              <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+                <Himage />
+              </div>
+            </p>
+            <hr />
+            <p className="mb-0">
+              Berikut tampilkan beberapa sistem yang kami kerjakan
+            </p>
+          </Alert>
+        </Col>
+      </Row>
+    </Container>
+
+    <Link to="/project/" style={{ marginRight: '20px' }}>Project</Link>
     <Link to="/contact/" style={{ marginRight: '20px' }}>Contact</Link>
     <Link to="/about/">About</Link>
   </Layout>
