080332c2aa881cdaf215fa567944bea36a8e9357
azware <anh.blackoni@gmail.com>
2020-06-04 14:38:08 +0700

    edit form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,11 +5,34 @@ import { PageProps, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+// For React-Bootstrap 
+import { Button,Form } from 'react-bootstrap'
+
 const ThirdPage = (props: PageProps) => (
   <Layout>
     <SEO title="Page three" />
-    <h1>This is a contact Form</h1>
-    <p>Welcome to ({props.path})</p>
+    <h1>Contact</h1>
+    <p>This is ({props.path})</p>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
